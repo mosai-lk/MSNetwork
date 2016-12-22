d3dffd623f99fe3a7b53857344b92ece90f626d2
leikai <leikai@mosainet.com>
2016-12-22 12:14:47 +0800

    ww

---
+++ MSNetwork/MSNet/MSNet.h
@@ -0,0 +1,19 @@
+//
+//  MSNet.h
+//  MSNet
+//
+//  Created by LK on 2016/12/22.
+//  Copyright © 2016年 Mosai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MSNet.
+FOUNDATION_EXPORT double MSNetVersionNumber;
+
+//! Project version string for MSNet.
+FOUNDATION_EXPORT const unsigned char MSNetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSNet/PublicHeader.h>
+
+
