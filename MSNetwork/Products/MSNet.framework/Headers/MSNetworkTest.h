88cf2e5454eab570437f9130b7dfcf6707c0295a
leikai <leikai@mosainet.com>
2016-12-22 12:16:45 +0800

    commit

---
+++ MSNetwork/Products/MSNet.framework/Headers/MSNetworkTest.h
@@ -0,0 +1,14 @@
+//
+//  MSNetworkTest.h
+//  MSNetwork
+//
+//  Created by LK on 2016/12/22.
+//  Copyright © 2016年 Mosai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSNetworkTest : NSObject
+- (void)test;
+
+@end
